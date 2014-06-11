960780d4641dcb2464a3cacd8589ba86b3474c28
Kevin Mitchell <kevin@develpr.com>
2014-06-10 23:45:01 -0700

    install zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,7 +48,11 @@ cd /var/www
 npm install -g bower
 npm install -g grunt-cli
 
-
+sudo apt-get install zsh
+wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
+sudo wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
+chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
 #install redis
 cd ~
 sudo wget http://download.redis.io/redis-stable.tar.gz
