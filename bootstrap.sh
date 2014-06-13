b024c6417fb15f85fc3d585a5596ca361c725a73
develpr <kevin@develpr.com>
2014-06-12 22:43:20 -0700

    Change to 14.04 Trusty with bootstrap modifications

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get install software-properties-common python-software-properties -y
+sudo apt-get install software-properties-common python-software-properties -y --force-yes
 
 sudo add-apt-repository ppa:chris-lea/node.js -y
 sudo add-apt-repository ppa:ondrej/php5 -y
@@ -13,21 +13,30 @@ sudo apt-get install -y unzip vim git-core curl wget build-essential python-soft
 sudp apt-get install git -y
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
-sudo apt-get install apache2 -y
-sudo apt-get install php5-mysql -y
+sudo apt-get -y install mysql-server --force-yes
 
-
-sudo apt-get install php5-fpm php5-cli php5-cgi php5-curl php5-gd -y
-sudo apt-get install php5-mcrypt -y
+sudo apt-get install apache2 -y --force-yes
+sudo apt-get install php5-mysql -y --force-yes
+sudo apt-get install php5-cli -y --force-yes
+sudo apt-get install php5-fpm -y --force-yes
+sudo apt-get install php5-curl -y --force-yes
+sudo apt-get install php5-gd -y --force-yes
+sudo apt-get install php5-cgi  --force-yes
+sudo apt-get install php5-mcrypt -y --force-yes
 
 #Install/configure xdebug
-sudo apt-get install php5-xdebug -y
+sudo apt-get install php5-xdebug -y --force-yes
 sudo echo "xdebug.remote_enable = 1" >> /etc/php5/fpm/conf.d/xdebug.ini
 sudo echo "xdebug.remote_host = 10.0.2.2" >> /etc/php5/fpm/conf.d/xdebug.ini
 sudo echo "xdebug.remote_port=9000" >> /etc/php5/fpm/conf.d/xdebug.ini
 sudo echo "xdebug.remote_handler=dbgp" >> /etc/php5/fpm/conf.d/xdebug.ini
 sudo echo "xdebug.idekey=PHPSTORM" >> /etc/php5/fpm/conf.d/xdebug.ini
+
+sudo echo "xdebug.remote_enable = 1" >> /etc/php5/fpm/conf.d/20-xdebug.ini
+sudo echo "xdebug.remote_host = 10.0.2.2" >> /etc/php5/fpm/conf.d/20-xdebug.ini
+sudo echo "xdebug.remote_port=9000" >> /etc/php5/fpm/conf.d/20-xdebug.ini
+sudo echo "xdebug.remote_handler=dbgp" >> /etc/php5/fpm/conf.d/20-xdebug.ini
+sudo echo "xdebug.idekey=PHPSTORM" >> /etc/php5/fpm/conf.d/20-xdebug.ini
 
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
@@ -41,9 +50,9 @@ sudo service php5-fpm restart
 sudo a2ensite default
 sudo a2enmod rewrite
 sudo service apache2 restart
-sudo apt-get install libapache2-mod-php5 -y
-sudo apt-get install npm -y
-sudo apt-get install nodejs -y
+sudo apt-get install libapache2-mod-php5 -y --force-yes
+sudo apt-get install npm -y --force-yes
+sudo apt-get install nodejs -y --force-yes
 cd /var/www
 npm install -g bower
 npm install -g grunt-cli
